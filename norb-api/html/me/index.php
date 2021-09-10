abb9a133f6a0a6320855a8177c9ecaee0d3610d3
Deploy Runner <ci-servicepanelng-deploy@norbert-ruehl.de>
2021-09-10 07:38:03 +0000

    [CI] auf master nur noch pushen

--- norb-api/html/me/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../../lib/CommonGatewayInterfaces/CGI_me.php';
-
-use norb_api\CommonGatewayInterfaces\CGI_me;
-
-new CGI_me();
--- norb-api/html/me/ldap/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../../../lib/CommonGatewayInterfaces/CGI_LDAPme.php';
-
-use norb_api\CommonGatewayInterfaces\CGI_LDAPme;
-
-new CGI_LDAPme();
