518db3fac6b43bc2510e073b98450d495a8df141
Deploy Runner <ci-servicepanelng-deploy@norbert-ruehl.de>
2021-12-22 13:34:45 +0000

    [CI] refactored CGI -> CommonGatewayInterfaces

--- html/ldap/InetOrgPerson/index.php
+++ html/ldap/InetOrgPerson/index.php
@@ -13,7 +13,7 @@
 ?>
 <?php
 
-require_once __DIR__ . '/../../../core/CGI/CGI_LDAP_InetOrgPerson.php';
+require_once __DIR__ . '/../../../core/CommonGatewayInterfaces/CGI_LDAP_InetOrgPerson.php';
 
 use servicepanel_ng\CGI_LDAP_InetOrgPerson;
 
--- html/ldap/PosixGroup/index.php
+++ html/ldap/PosixGroup/index.php
@@ -13,7 +13,7 @@
 ?>
 <?php
 
-require_once __DIR__ . '/../../../core/CGI/CGI_LDAP_PosixGroup.php';
+require_once __DIR__ . '/../../../core/CommonGatewayInterfaces/CGI_LDAP_PosixGroup.php';
 
 use servicepanel_ng\CGI_LDAP_PosixGroup;
 
